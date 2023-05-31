d729e197566ab09f341cc334768e2c9930cdaabf
Peter Boin <peter.boin@allumeenergy.com.au>
2023-05-31 21:26:33 +1000

    clean old backups

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A script to perform incremental backups using rsync
 # source: https://linuxconfig.org/how-to-create-incremental-backups-using-rsync-on-linux
-set -xueo pipefail
+set -ueo pipefail
 cd $(dirname $0)
 
 # Paths
@@ -12,6 +12,7 @@ readonly DATETIME="$(date '+%Y%m%d_%H%M%S')"
 readonly BACKUP_PATH="$BACKUP_DIR/$DATETIME"
 readonly LATEST_LINK="$BACKUP_DIR/latest"
 
+# ----- Create Backup
 mkdir -p "${BACKUP_DIR}"
 
 # Perform Sync
@@ -24,3 +25,12 @@ rsync -av --delete \
 # Cleanup
 rm -rf "$LATEST_LINK"
 ln -s "$BACKUP_PATH" "$LATEST_LINK"
+
+# ----- Clean Old Backups
+# Remove backups older than 30 days
+find "$BACKUP_DIR" \
+    -mindepth 1 \
+    -maxdepth 1 \
+    -type d \
+    -mtime +30 \
+    -exec rm -rf {} \;
