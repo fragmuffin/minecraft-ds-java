86a732a24a3a4315ed9d2821ca49596ae7e67a8c
Peter Boin <peter.boin@allumeenergy.com.au>
2023-07-24 09:58:02 +1000

    clean backups based on folder name - instead of mtime (modification date)

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -17,20 +17,24 @@ mkdir -p "${BACKUP_DIR}"
 
 # Perform Sync
 rsync -av --delete \
-  "$SOURCE_DIR/" \
-  --link-dest "$LATEST_LINK" \
-  --exclude=".cache" \
-  "$BACKUP_PATH/"
+    "$SOURCE_DIR/" \
+    --link-dest "$LATEST_LINK" \
+    --exclude=".cache" \
+    "$BACKUP_PATH/"
 
 # Cleanup
 rm -rf "$LATEST_LINK"
 ln -s "$BACKUP_PATH" "$LATEST_LINK"
 
 # ----- Clean Old Backups
-# Remove backups older than 30 days
-find "$BACKUP_DIR" \
-    -mindepth 1 \
-    -maxdepth 1 \
-    -type d \
-    -mtime +30 \
-    -exec rm -rf {} \;
+cutoff_epoch=$(date -d "-30 days" +%s)
+for bak_path in $(find "$BACKUP_DIR" -maxdepth 1 -mindepth 1 -type d) ; do
+    bak_name=$(basename $bak_path)
+    if ( echo $bak_name | grep -qP '^\d{8}_\d{6}$' ) ; then
+        bak_dt=$(echo $bak_name | sed -r 's/(....)(..)(..)_(..)(..)(..)/\1-\2-\3 \4:\5:\6/')
+        bak_epoch=$(date -d "$bak_dt" +%s)
+        if [ $bak_epoch -lt $cutoff_epoch ]; then
+            rm -rf "$bak_path"
+        fi
+    fi
+done
