76083f5c2b1ec0205199e4d912ee8ae843466598
Peter Boin <peter.boin@allumeenergy.com.au>
2023-10-12 20:28:50 +1100

    added log_types, player event focussed - exhaustive death message checks - join, leave, and advancement

--- notify/lib/logs.py
+++ notify/lib/logs.py
@@ -7,6 +7,8 @@ import os
 import datetime
 import logging
 
+import log_types
+
 log = logging.getLogger(__name__)
 
 
@@ -127,115 +129,9 @@ class LogMessage:
             if (logentry := cls.from_string(line=line, date=date)):
                 yield logentry
 
-    IDENTITY_MAP = {  # fmt: {<key>: <regex>, ...}
-        # Joining / Leaving
-        'joined': re.compile(r'^\s*(?P<name>[\w ]+?) joined the game\s*$'),
-        'logged_in': re.compile(r'''^\s*
-            (?P<name>[\w ]+?)\[.*?\]\s*     # Username (TODO: from IP)
-            logged in with entity id (?P<id>\d+) at\s*  # entity ID
-            \(\[(?P<world>\w+)\]\s*         # World name
-                (?P<x>\d+(\.\d+)?)\s*,\s*   # coord: x
-                (?P<y>\d+(\.\d+)?)\s*,\s*   # coord: y
-                (?P<z>\d+(\.\d+)?)\s*,?\s*  # coord: z
-            \)
-        \s*$''', re.VERBOSE),
-        'disconnected': re.compile(r'^\s*(?P<name>[\w ]+?) lost connection: (?P<reason>.*?)\s*$'),
-        'left': re.compile(r'^\s*(?P<name>[\w ]+?) left the game\s*$'),
-
-        # Events
-        'advancement': re.compile(r'^\s*(?P<name>[\w ]+?)\s*has made the advancement \[(?P<type>.*)\]\s*$'),
-
-        # Deaths : https://minecraft.fandom.com/wiki/Death_messages
-        'drowned': re.compile(r'^\s*(?P<name>[\w ]+?)\s*drowned\s*$'),
-        'slain': re.compile(r'^\s*(?P<name>[\w ]+?)\s*was slain by\s*(?P<other>.*?)(\s+using (?P<weapon>.*?))?\s*$'),,
-        'lava': re.compile(r'^\s*(?P<name>[\w ]+?)\s*tried to swim in lava\s*$'),
-        'fell': re.compile(r'^\s*(?P<name>[\w ]+?)\s*fell from a high place\s*$'),
-        'stung': re.compile(r'^\s*(?P<name>[\w ]+?)\s*was stung to death(by\s*(?P<other>.*?)(\s+using (?P<weapon>.*?))?)?\s*$'),
-        'shriek': re.compile(r'^\s*(?P<name>[\w ]+?)\s*was obliterated.*$'),
-        'shot': re.compile(r'^\s*(?P<name>[\w ]+?)\s*was shot by\s*(?P<other>.*?)(\s+using (?P<weapon>.*?))?\s*$'),
-        
-        '_template': re.compile(r'^\s*(?P<name>[\w ]+?)\s*\s*$'),  # template
-
-        # there are a lot... perhaps rethink this method
-
-    }
-
     @property
     def type(self):
         """Identify a log message as a particular type"""
         if self._type is None:
-            for (tag, regex) in self.IDENTITY_MAP.items():
-                if (match := regex.search(self.message)):
-                    self._type = tag
-                    break
-                    # note: match not used; so (?P<names>) are lost, useful?
-            else:
-                self._type = 'UNKNOWN'
+            (self._type, _) = log_types.detect(self.message)
         return self._type
-
-
-if __name__ == '__main__':
-    import unittest
-    
-    class TestLogMessages(unittest.TestCase):
-        def assertLogType(self, text, exp_type):
-            today = datetime.date.today()
-            obj = LogMessage.from_string(text, date=today)
-            self.assertEqual(obj.type, exp_type)
-        
-        def test_joined(self):
-            self.assertLogType(
-                "[12:15:23] [Server thread/INFO]: FraggyMuffin joined the game",                
-                'joined'
-            )
-    
-        def test_logged_in(self):
-            self.assertLogType(
-                "[12:15:23] [Server thread/INFO]: FraggyMuffin[/172.25.0.1:39356] logged in with entity id 27 at ([world]9.5, 80.0, -2.5)",
-                'logged_in'
-            )
-        def test_disconnected(self):
-            self.assertLogType(
-                "[12:17:11] [Server thread/INFO]: FraggyMuffin lost connection: Disconnected",
-                'disconnected'
-            )
-        
-        def test_left(self):
-            self.assertLogType(
-                "[12:17:11] [Server thread/INFO]: FraggyMuffin left the game",
-                'left'
-            )
-        
-        def test_advancement(self):
-            self.assertLogType(
-                "[12:17:11] [Server thread/INFO]: FraggyMuffin has made the advancement [Monster Hunter]",
-                'advancement'
-            )
-        
-        def test_drowned(self):
-            self.assertLogType(
-                "[02:13:12] [Server thread/INFO]: Dude drowned",
-                'drowned'
-            )
-        
-        def test_slain(self):
-            self.assertLogType(
-                "[02:13:12] [Server thread/INFO]: Dude was slain by AnotherDude",
-                'slain'
-            )
-        
-        def test_lava(self):
-            self.assertLogType(
-                "[02:23:19] [Server thread/INFO]: SomeDude tried to swim in lava",
-                'lava'
-            )
-        
-        def test_fell(self):
-            self.assertLogType(
-                "[02:13:12] [Server thread/INFO]: SomeDude fell from a high place",
-                'fell'
-            )
-        
-
-
-    unittest.main()
